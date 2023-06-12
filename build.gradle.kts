9d97d92bf30bb09ac1c61fd01fec9d8b37cd5ab9
JouTao <1832595297@qq.com>
2023-06-12 10:48:44 +0800

    版本回退2021.3

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
   id("java")
-  id("org.jetbrains.intellij") version "1.13.0"
+  id("org.jetbrains.intellij") version "1.10.1"
 }
 
 group = "com.wwr"
@@ -11,27 +11,29 @@ repositories {
 }
 
 dependencies {
-  implementation ("io.swagger.core.v3:swagger-models:2.2.8")
-  implementation ("io.github.openfeign:feign-core:12.2")
-  implementation ("io.github.openfeign:feign-gson:12.2")
+  implementation ("io.swagger.core.v3:swagger-models:2.2.6")
+  implementation ("io.github.openfeign:feign-core:12.1")
+  implementation ("io.github.openfeign:feign-gson:12.1")
   implementation ("io.github.openfeign.form:feign-form:3.8.0")
   implementation ("org.slf4j:slf4j-api:2.0.5")
   // lombok
-  compileOnly ("org.projectlombok:lombok:1.18.26")
-  annotationProcessor ("org.projectlombok:lombok:1.18.26")
-  testCompileOnly ("org.projectlombok:lombok:1.18.26")
-  testAnnotationProcessor ("org.projectlombok:lombok:1.18.26")
-  testImplementation ("org.junit.jupiter:junit-jupiter-api:5.9.2")
-  testRuntimeOnly ("org.junit.jupiter:junit-jupiter-engine:5.9.2")
+  compileOnly ("org.projectlombok:lombok:1.18.24")
+  annotationProcessor ("org.projectlombok:lombok:1.18.24")
+  testCompileOnly ("org.projectlombok:lombok:1.18.24")
+  testAnnotationProcessor ("org.projectlombok:lombok:1.18.24")
+  testImplementation ("org.junit.jupiter:junit-jupiter-api:5.9.0")
+  testRuntimeOnly ("org.junit.jupiter:junit-jupiter-engine:5.9.0")
 
   implementation ("cn.hutool:hutool-http:5.8.15")
   implementation ("cn.hutool:hutool-json:5.8.15")
+
+  implementation ("org.yaml:snakeyaml:1.29")
 }
 
 // Configure Gradle IntelliJ Plugin
 // Read more: https://plugins.jetbrains.com/docs/intellij/tools-gradle-intellij-plugin.html
 intellij {
-  version.set("2023.1.2")
+  version.set("2021.3")
   type.set("IC") // Target IDE Platform
   sandboxDir.set("${project.rootDir}/.sandbox")
   updateSinceUntilBuild.set(false)
@@ -41,12 +43,12 @@ intellij {
 tasks {
   // Set the JVM compatibility versions
   withType<JavaCompile> {
-    sourceCompatibility = "17"
-    targetCompatibility = "17"
+    sourceCompatibility = "11"
+    targetCompatibility = "11"
   }
 
   patchPluginXml {
-    sinceBuild.set("201")
+    sinceBuild.set("213")
     untilBuild.set("231.*")
   }
 
